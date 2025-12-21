565e16796ccc80bf01b5dcc9d6fea025cca3b93e
eberegovaya <beregovayaelena123@gmail.com>
2025-12-21 20:55:34 +0300

    Улучшили логику main и doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,12 +4,13 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("11111111");
+        System.out.println("-----");
     }
 
     public static void doSomething(){
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
+        System.out.println(0);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
