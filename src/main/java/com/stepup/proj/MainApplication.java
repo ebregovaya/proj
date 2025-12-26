890ece1caf607a7301468c6c3a7ade601f065e2e
eberegovaya <beregovajaelena123@gmail.com>
2025-12-26 09:38:24 +0300

    Изменили логику doSomething()26.12

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,8 +9,8 @@ public class MainApplication {
 
     public static void doSomething(){
         System.out.println(1);
-        System.out.println(0);
-        System.out.println(0);
+        System.out.println(1);
+        System.out.println(1);
         System.out.println(1);
     }
 }
